442129934cb485bb8e387e900a47c770f268502c
qinjo <60745301+qinjo@users.noreply.github.com>
2022-05-18 10:36:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Tab/>
+  </div>
+</template>
+  
+  
+<script setup lang='ts'>
+import Tab from './components/tab/index.vue';
+defineOptions({
+  name: 'App'
+})
+</script>
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+}
+</style>
---
+++ src/components/tab/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tab-animation">
+    <ul>
+      <li
+        v-for="(tab) in tabList"
+        @click="currentTab = tab.key"
+        :class="tabClass(tab.key)"
+        :key="tab.key">
+          {{tab.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+  
+  
+<script setup lang='ts'>
+import { ref } from 'vue';
+import './style.scss';
+
+defineOptions({
+  name: 'TabAnimation'
+})
+const currentTab = ref('LINE');
+const tabList = [
+  {
+    title: 'LINE',
+    key: 'LINE'
+  },
+  {
+    title: 'FAMILY SERVICE',
+    key: 'SERVICE'
+  },
+  {
+    title: 'FAMILY SERVICE1',
+    key: 'SERVICE2'
+  },
+]
+const tabClass = (key: string) => {
+  return currentTab.value === key ? 'is-active' : 'default';
+}
+</script>
